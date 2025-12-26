63a1f45ebd3fe0b8100f20b32a4d18cdf1e9bf33
unknown <houssin123.dev@gmail.com>
2025-12-27 01:57:39 +0300

    fix frontend url

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponseRedirect
 from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-
+from django.conf import settings
 
 class GoogleLoginViewSet(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://localhost:3000/api/auth/callback/google"
+    callback_url = f"{settings.FRONTEND_URL}/api/auth/callback/google"
     client_class = OAuth2Client
 
 
