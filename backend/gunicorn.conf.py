e4a863aad4531bcab7606544b43117a64b9718b0
unknown <houssin123.dev@gmail.com>
2025-12-26 23:19:15 +0300

    remove cert vars

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -8,5 +8,5 @@ workers = multiprocessing.cpu_count() * 2 + 1
 worker_connections = 1000
 threads = 4
 
-certfile = "/etc/letsencrypt/live/ruwadacademy.com/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/ruwadacademy.com/privkey.pem"
+# certfile = "/etc/letsencrypt/live/ruwadacademy.com/fullchain.pem"
+# keyfile = "/etc/letsencrypt/live/ruwadacademy.com/privkey.pem"
