a091cc19fbd3243d2a2b671f671029de0ae3eb13
AIT ALI EL HOSAYN <44384710+housUnus@users.noreply.github.com>
2025-12-28 10:31:34 +0300

    added logs to gunicorn

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -8,5 +8,9 @@ workers = multiprocessing.cpu_count() * 2 + 1
 worker_connections = 1000
 threads = 4
 
-# certfile = "/etc/letsencrypt/live/ruwadacademy.com/fullchain.pem"
-# keyfile = "/etc/letsencrypt/live/ruwadacademy.com/privkey.pem"
+accesslog = "-"        # stdout
+errorlog = "-"         # stderr
+loglevel = "info"
+
+capture_output = True
+enable_stdio_inheritance = True
