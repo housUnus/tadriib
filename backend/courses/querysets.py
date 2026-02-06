b85c904c0aed22ecf13fb7e6228449efb5e20433
unknown <houssin123.dev@gmail.com>
2026-02-07 02:42:19 +0300

    ratings improve

--- backend/courses/querysets.py
+++ backend/courses/querysets.py
@@ -2,14 +2,14 @@
 from django.db import models
 from django.db.models import Avg, Count
 from courses.contents.video.models import Video
-
+from ratings.constants import RatingStatus
 
 class CourseQuerySet(models.QuerySet):
     def with_stats(self):
         return self.annotate(
             # total_students=Count("enrollments", distinct=True),
-            total_reviews=Count("ratings", distinct=True),
-            average_rating=Avg("ratings__value"),
+            total_reviews=Count("ratings", distinct=True, filter=models.Q(ratings__status=RatingStatus.APPROVED)),
+            average_rating=Avg("ratings__value", filter=models.Q(ratings__status=RatingStatus.APPROVED)),
         )
         
     def with_main_preview(self):
