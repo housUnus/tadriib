de9b5721792471ef561d0a8cde4c3d5f51fc5d0d
unknown <houssin123.dev@gmail.com>
2026-02-15 21:33:33 +0300

    little fixes

--- frontend/src/components/common/Modal/modal.tsx
+++ frontend/src/components/common/Modal/modal.tsx
@@ -24,6 +24,13 @@ type ModalProps = UseModalOptions & {
   close: () => void
 }
 
+const sizeClasses = {
+  sm: "max-w-1/3",
+  md: "max-w-1/2",
+  lg: "max-w-2/3",
+  full: "max-w-full",
+}
+
 export function Modal({
   isOpen,
   close,
@@ -34,7 +41,7 @@ export function Modal({
 }: ModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={close}>
-      <DialogContent className={` w-full max-w-${size === "sm" ? "1/3" : size === "md" ? "1/2" : size === "lg" ? "2/3" : size === "full" ? "full" : "1/2"} rounded-lg bg-white dark:bg-darkgray p-6 shadow-md dark:dark-shadow-md`}
+      <DialogContent className={`w-full ${sizeClasses[size] || sizeClasses.md} rounded-lg bg-white dark:bg-darkgray p-6 shadow-md dark:dark-shadow-md`}
       >
         <ScrollArea className="max-h-[90vh]">
           <div className="pt-3">
