bca8cc21eeb4600d114bb7046568aceb029f35c3
unknown <houssin123.dev@gmail.com>
2026-04-18 22:23:28 +0300

    attempt to fix certbot

--- deploy.sh
+++ deploy.sh
@@ -19,3 +19,5 @@ echo "==> Updating containers"
 docker compose up -d --remove-orphans
 
 echo "==> Deployment completed successfully!"
+
+# docker compose restart nginx : IS NEEDED AT FIRST TIME TO MOUNT RENEW SCRIPT
